e410228113e57334cf9b84b0bfa07ee4867f089b
Vincent C. Mader <vincent.mader@icloud.com>
2023-06-05 18:25:26 +0200

    implemented generation of QR code image files from URL string (given as CLI argument)

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
+test:
+	cd ./bin && ./run "https://georgs-punkte.de"
 run:
 	cd ./bin && ./run
-
 setup:
 	cd ./bin && ./setup
--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
+from pathlib import Path
+import sys
+
+import qrcode
+import qrcode.image.svg
+
+
+def get_url_from_cli_args():
+    cli_args = sys.argv
+    try:
+        url = cli_args[1]
+        return url
+    except IndexError:
+        raise Exception("You have to specify a url!")
+
+
 def main():
-    print("Hello, world!")
+    # Get URL from command line arguments.
+    url = get_url_from_cli_args()
+    # Generate QR code image.
+    img = qrcode.make(url)
+    # Define path to QR code image file. 
+    filename = url.replace('/', '_')
+    filename = f"{filename}.png"
+    path_to_file = Path("..", "out", filename)
+    # Save QR code image to disk.
+    with open(path_to_file, "wb") as qr:
+        img.save(qr)
 
 
 if __name__ == "__main__":
